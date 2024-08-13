049420c3d3370f4377db9f7dee323010da98f77a
Alexandru2401 <a.toma231@yahoo.com>
2024-08-13 09:57:03 +0300

    randomize answers and add UI for the end of the quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
 import QUESTIONS from "../Questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswer) => {
       return [...prevUserAnswer, selectedAnswer];
     });
   }
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="" />
+        <h2>Quiz completed</h2>
+      </div>
+    );
+  }
+
+  //Randomise the answers
+  const shuffledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswer.sort(() => Math.random() - 0.5);
+  
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswer.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
